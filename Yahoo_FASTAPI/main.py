ad3a682bded4ec326f85835790367ea0c197a0d4
Cody Scott <jcodyscott@gmail.com>
2022-01-24 15:32:22 -0500

    update

--- Yahoo_FASTAPI/main.py
+++ Yahoo_FASTAPI/main.py
@@ -1,3 +1,4 @@
+from audioop import add
 from fastapi import Depends, FastAPI
 from starlette.middleware.sessions import SessionMiddleware
 
@@ -11,6 +12,12 @@ from models import team_model, player_model
 
 from dotenv import load_dotenv
 import os
+
+import json
+from pathlib import Path
+
+import datetime
+
 load_dotenv()
 league_id = os.environ['LEAGUE_ID']
 base_url = f"https://fantasysports.yahooapis.com/fantasy/v2/league/{league_id}"
@@ -56,3 +63,57 @@ async def yahoo_players(start_num: Optional[int]=0):
     )
     rf = xmltodict.parse(r.content)['fantasy_content']['league']['players']['player']
     return rf
+
+@app.get('/roster')
+async def get_roster(team_id: int, roster_date: Optional[datetime.date]=None):
+    token = await get_token()
+    url = f"https://fantasysports.yahooapis.com/fantasy/v2/team/{league_id}.t.{team_id}/roster/players"
+    
+    if roster_date is not None:
+        date_str = roster_date.strftime("%Y-%m-%d")
+        url += f";date={date_str}"
+
+    r = await oauth.yahoo.get(
+        url,
+        token=token
+    )
+    rf = xmltodict.parse(r.content)
+    Path('data.json').write_text(json.dumps(r.content, indent=4))
+    return r.content
+
+
+async def post_changes(team_id: int, add_player_id: Optional[int]=None, drop_player_id: Optional[int]=None):
+    if all([add_player_id, drop_player_id]):
+        _tp = "add/drop"
+    elif add_player_id is not None:
+        _tp = "add"
+    elif drop_player_id is not None:
+        _tp = "drop"
+    else:
+        return "Error!"
+
+    _aa = format_player_xml(team_id, add_player_id, "add") if add_player_id is not None else ""
+    _dd = format_player_xml(team_id, drop_player_id, "drop") if drop_player_id is not None else ""
+    
+    xml_data = f"""<?xml version='1.0'?>
+                <fantasy_content>
+                    <transaction>
+                        <type>{_tp}</type>
+                        <players>
+                        {_aa}
+                        {_dd}
+                        </players>
+                    </transaction>
+                </fantasy_content>"""
+
+
+    _url = "https://fantasysports.yahooapis.com/fantasy/v2/league/transactions"
+
+def format_player_xml(team_id: int, player_id: int, add_drop: str):
+    return f"""<player>
+                    <player_key>nhl.p.{player_id}</player_key>
+                    <transaction_data>
+                    <type>{add_drop}</type>
+                    <destination_team_key>nhl.l.{league_id}.t.{team_id}</destination_team_key>
+                    </transaction_data>
+                </player>"""
